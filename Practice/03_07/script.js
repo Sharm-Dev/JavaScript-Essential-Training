6adc5186c039109fdd3ddeea7ec28c24ef178227
Sharm-Dev <sharmainetaylor@ymail.com>
2021-12-28 18:27:58 +0000

    Practice: Build a new object completed

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,26 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const cupboard = {
+    jar: "Sugar",
+    packet: "Pasta",
+    tin: "Beans",
+    tub: {
+        chocolate: true,
+        sweets: false,
+        mints: false,
+    },
+    tin: {
+        snack1: "chocolate",
+        snack2: "jaffas",
+    },
+};
+
+console.log("The cupboard object", cupboard);
+console.log("tub", cupboard.tub);
+console.log("tin", cupboard.tin.snack1);
+console.log("packet:", cupboard["packet"]);
+
+let jarValue = "jar"
+console.log("The jar:", cupboard[jarValue])
